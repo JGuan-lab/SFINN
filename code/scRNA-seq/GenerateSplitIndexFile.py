db25a6be886627a6065cd1d4dde0df33c678f421
JGuan-lab <64457671+JGuan-lab@users.noreply.github.com>
2024-01-24 10:15:41 +0800

    Update GenerateSplitIndexFile.py

--- code/scRNA-seq/GenerateSplitIndexFile.py
+++ code/scRNA-seq/GenerateSplitIndexFile.py
@@ -1,11 +1,3 @@
-'''
-功能:输入Groundtrue列表,只保留含有0,1关系的基因对,同时为该候选基因对列表生成区间划分索引列表文件
-调用:python GenerateSplitIndexFile.py --GroundTrueFilePath xx --SplitIndexSavePath xx
-参数:
---GroundTrueFilePath  GroundTrue数据所在路径
---SplitIndexSavePath  区间划分索引列表保存路径
-
-'''
 import pandas as pd
 from numpy import *
 import json, re,os, sys
